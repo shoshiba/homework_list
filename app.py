f99ed3cda6c1658b3fa62eac9a009dba12bb32bd
sig49san <sibahara3@gmail.com>
2024-06-06 14:45:26 +0900

    リファクタリングした。

--- app.py
+++ app.py
@@ -1,98 +1,71 @@
 import streamlit as st
-from data_cloling import crawl_and_save_iidex_data
 import pandas as pd
+import re
+from data_cloling import crawl_and_save_iidex_data
 
 # Streamlitアプリの設定
 st.title('IIDX DP Homework確認用ツール')
 
-#iidx_id_me = st.text_input('自分のIIDX IDを入れてください:', '16548190')
-#
-#if st.button('Fetch Data',key='fetch_data_me'):
-#    df_me = crawl_and_save_iidex_data(iidx_id_me)
-#    st.dataframe(df_me)
-#
-#iidx_id_rival = st.text_input('比較したい人のIIDX IDを入れてください:', '55344136')
-#
-#if st.button('Fetch Data',key='fetch_data_rival'):
-#    df_rival = crawl_and_save_iidex_data(iidx_id_rival)
-#    st.dataframe(df_rival)
+# IIDX ID入力
+iidx_ids = {
+    'me': st.text_input('自分のIIDX IDを入れてください:', '16548190'),
+    'rival1': st.text_input('比較したい人のIIDX IDを入れてください1:', '55344136'),
+    'rival2': st.text_input('比較したい人のIIDX IDを入れてください2:', '42936531'),
+    'rival3': st.text_input('比較したい人のIIDX IDを入れてください3:', '48537824')
+}
 
+def fetch_data(iidx_id):
+    """指定されたIIDX IDのデータを取得する"""
+    return crawl_and_save_iidex_data(iidx_id)
 
+def extract_percentage(performance):
+    """Performanceからパーセンテージを抽出"""
+    match = re.search(r'(\d+\.\d+%)', performance)
+    return match.group(1) if match else '0.0%'
 
-iidx_id_me = st.text_input('自分のIIDX IDを入れてください:', '16548190')
-iidx_id_rival1 = st.text_input('比較したい人のIIDX IDを入れてください1:', '55344136')
-iidx_id_rival2 = st.text_input('比較したい人のIIDX IDを入れてください2:', '42936531')
-iidx_id_rival3 = st.text_input('比較したい人のIIDX IDを入れてください3:', '48537824')
+def calculate_rank(row):
+    """スコアに基づいて順位を計算する"""
+    scores = [row['Me'], row['Rival1'], row['Rival2'], row['Rival3']]
+    return sorted(scores, reverse=True).index(row['Me']) + 1
 
-fetch_score_data = st.button('Fetch Score Data')
-#fetch_rival1 = st.button('Fetch Rival1 Data')
-#fetch_rival2 = st.button('Fetch Rival2 Data')
-#fetch_rival3 = st.button('Fetch Rival3 Data')
+def create_comparison_dataframe(dfs):
+    """比較用のデータフレームを作成する"""
+    comparison = pd.DataFrame({
+        'Level': dfs['me']['Level'],
+        'Title': dfs['me']['Title'],
+        'Me': dfs['me']['Details_Number'],
+        'MePer': dfs['me']['Performance'],
+        'Rival1': dfs['rival1']['Details_Number'],
+        'Rival2': dfs['rival2']['Details_Number'],
+        'Rival3': dfs['rival3']['Details_Number'],
+        'vsRival1': dfs['me']['Details_Number'] - dfs['rival1']['Details_Number'],
+        'vsRival2': dfs['me']['Details_Number'] - dfs['rival2']['Details_Number'],
+        'vsRival3': dfs['me']['Details_Number'] - dfs['rival3']['Details_Number'],
+    }).fillna(0)
+    comparison['Rank'] = comparison.apply(calculate_rank, axis=1)
+    comparison['Me_Per'] = comparison['MePer'].apply(extract_percentage)
+    return comparison[['Level', 'Title', 'Rank', 'Me', 'Me_Per', 'Rival1', 'Rival2', 'Rival3', 'vsRival1', 'vsRival2', 'vsRival3']]
 
-if fetch_score_data:
-    df_me = crawl_and_save_iidex_data(iidx_id_me)
-    st.session_state['df_me'] = df_me
-    st.subheader('自分のデータ')
-    st.dataframe(df_me)
-    df_rival1 = crawl_and_save_iidex_data(iidx_id_rival1)
-    st.session_state['df_rival1'] = df_rival1
-    st.subheader('ライバル1のデータ')
-    st.dataframe(df_rival1)
-    df_rival2 = crawl_and_save_iidex_data(iidx_id_rival2)
-    st.session_state['df_rival2'] = df_rival2
-    st.subheader('ライバル2のデータ')
-    st.dataframe(df_rival2)
-    df_rival3 = crawl_and_save_iidex_data(iidx_id_rival3)
-    st.session_state['df_rival3'] = df_rival3
-    st.subheader('ライバル3のデータ')
+# スコアデータの取得
+if st.button('Fetch Score Data'):
+    dfs = {key: fetch_data(iidx_id) for key, iidx_id in iidx_ids.items()}
+    for key, df in dfs.items():
+        st.session_state[f'df_{key}'] = df
 
 # 比較データの表示
-if 'df_me' in st.session_state and 'df_rival1' in st.session_state:
-    df_me = st.session_state['df_me']
-    df_rival1 = st.session_state['df_rival1']
-    df_rival2 = st.session_state['df_rival2']
-    df_rival3 = st.session_state['df_rival3']
-    
-    comparison = pd.DataFrame({
-        'Level': df_me['Level'],
-        'Title': df_me['Title'],
-        'Me': df_me['Details_Number'],
-        'Rival1': df_rival1['Details_Number'],
-        'Rival2': df_rival2['Details_Number'],
-        'Rival3': df_rival3['Details_Number'],
-        'vsRival1': df_me['Details_Number'] - df_rival1['Details_Number'],
-        'vsRival2': df_me['Details_Number'] - df_rival2['Details_Number'],
-        'vsRival3': df_me['Details_Number'] - df_rival3['Details_Number'],
-    }).fillna(0)  # データの数が一致しない場合は0で埋める
+if all(f'df_{key}' in st.session_state for key in iidx_ids.keys()):
+    dfs = {key: st.session_state[f'df_{key}'] for key in iidx_ids.keys()}
+    comparison = create_comparison_dataframe(dfs)
 
-# 各曲の順位を計算
-    def calculate_rank(row):
-        scores = [row['Me'], row['Rival1'], row['Rival2'], row['Rival3']]
-        return sorted(scores, reverse=True).index(row['Me']) + 1
-    comparison['Rank'] = comparison.apply(calculate_rank, axis=1)
-
-    comparison = comparison[['Level', 'Title','Rank', 'Me', 'Rival1', 'Rival2', 'Rival3', 'vsRival1', 'vsRival2', 'vsRival3']]
-    st.subheader('ライバルスコアとの比較')
-    # フィルタリングチェックボックス
-    filter_checkbox = st.checkbox('全員が未プレイの曲を除外')
-
+    # レベル別フィルター
     st.write('レベル別フィルター')
-    # レベルフィルタリングチェックボックス
-    if 'df_me' in st.session_state:
-        df_me = st.session_state['df_me']
-        levels = sorted(df_me['Level'].unique(), key=lambda x: int(x.lstrip('☆')))
-        selected_levels = []
-        
-        cols = st.columns(len(levels))
-        for i, level in enumerate(levels):
-            if cols[i].checkbox(level, value=True):
-                selected_levels.append(level)
-    else:
-        selected_levels = []
-    # レベルフィルタリングを適用
+    levels = sorted(dfs['me']['Level'].unique(), key=lambda x: int(x.lstrip('☆')))
+    selected_levels = [level for level in levels if st.checkbox(level, value=True)]
+
+    # フィルタリング適用
     comparison = comparison[comparison['Level'].isin(selected_levels)]
-    if filter_checkbox:
+    if st.checkbox('全員が未プレイの曲を除外'):
         comparison = comparison[(comparison['Me'] != 0) | (comparison['Rival1'] != 0) | (comparison['Rival2'] != 0) | (comparison['Rival3'] != 0)]
+    
+    st.subheader('ライバルスコアとの比較')
     st.dataframe(comparison)
-
-
--- data_cloling.py
+++ data_cloling.py
@@ -2,46 +2,47 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
-
-def crawl_and_save_iidex_data(iidx_id):
-# 指定されたURL
-
-    
-    url = f"http://ereter.net/iidxplayerdata/{iidx_id}/level"
-    # ページの内容を取得
-
+def crawl_iidx_data(iidx_id):
+    """
+    指定されたIIDX IDのデータをクローリングして返す
+    """
+    base_url = f"http://ereter.net/iidxplayerdata/{iidx_id}/level"
     data = []
-    for i in range(1,13):
-        urls = [url, "/", str(i)]
-        hoge = "".join(urls)
 
-        response = requests.get(hoge)
+    for i in range(1, 13):
+        url = f"{base_url}/{i}"
+        response = requests.get(url)
+        response.raise_for_status()
         soup = BeautifulSoup(response.content, "html.parser")
         
-        
-        table = soup.find("table",class_="table condensed")
-        
-        # テーブルデータをリストに変換
+        table = soup.find("table", class_="table condensed")
         rows = table.find_all("tr")
-        
+
         for row in rows:
             cols = row.find_all("td")
             if cols:
                 cols = [col.get_text(strip=True) for col in cols]
                 data.append(cols)
-        
-    # カラム名を定義（必要に応じて適切な名前を付ける）
+
+    return data
+
+def create_iidx_dataframe(data):
+    """
+    クローリングされたデータからDatatableを作成して返す
+    """
     column_names = ["Level", "Title", "Rank", "Details", "Performance", "Difficulty"]
-    
-    # Pandas DataFrameに変換
     df = pd.DataFrame(data, columns=column_names)
     
-    # "Details" カラムから数値を抽出し、新しいカラムとして追加
     df["Details"] = df["Details"].replace("", "0")
     df["Details_Number"] = df["Details"].str.extract(r'(\d+)').fillna(0).astype(int)
-    
-    # 不要なカラムがあれば削除（例として "Details" カラムを削除）
     df.drop(columns=["Details"], inplace=True)
-        
-        # データフレームを表示
+
+    return df
+
+def crawl_and_save_iidex_data(iidx_id):
+    """
+    IIDX IDに基づいてデータをクローリングし、Datatableを作成して返す
+    """
+    raw_data = crawl_iidx_data(iidx_id)
+    df = create_iidx_dataframe(raw_data)
     return df
